c1f16cf7101b47989fce66675b21dc24d59fc3fb
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-03-07 11:18:20 -0500

    Se creó la función keepAlive con el fin de saber si la aplicación se esta ejecutando o no

---
+++ keepAlive.js
@@ -0,0 +1,12 @@
+const app = require('express')()
+
+function keepAlive() {
+    app.get('/', (_, res) => {
+        res.send('Hello World', 5, 5)
+    })
+    app.listen(3000)
+}
+
+module.exports = {
+    keepALive
+}
