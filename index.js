2c3de3ee639940136e8d1b5c950bc54c03270ead
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-04-19 23:06:04 -0500

    Refactor keepAlive function to listen on port 3002 in index.js

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ client.once("ready", ready(client));
 client.on("interactionCreate", interactionCreate(client));
 client.on("guildCreate", guildCreate());
 client.on("guildDelete", guildDelete());
-app.listen(3000);
+app.listen(3002, () => {
+  console.log(`Server is running on port ${3002}`);
+});
 client.login(process.env["token"]);
