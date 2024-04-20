836decd6616768f00babbc9534000446b10e82d6
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-04-19 23:14:20 -0500

    Removed Express import

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { Client } = require("discord.js");
-const app = require("express")();
 require("dotenv").config();
-const { keepAlive } = require("./modules/keepAlive");
 const {
   ready,
   interactionCreate,
@@ -11,14 +9,8 @@ const {
 } = require("./modules/events");
 
 const client = new Client(clientOptions);
-app.get("/", (_req, res) => {
-  res.send("Hello World");
-});
 client.once("ready", ready(client));
 client.on("interactionCreate", interactionCreate(client));
 client.on("guildCreate", guildCreate());
 client.on("guildDelete", guildDelete());
-app.listen(3002, () => {
-  console.log(`Server is running on port ${3002}`);
-});
 client.login(process.env["token"]);
