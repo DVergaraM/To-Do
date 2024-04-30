25717f6ab621430e81a26b1b531d26792b78c226
DVergaraM <daniel.vergaram@urosario.edu.co>
2024-04-30 07:59:00 -0500

    Refactor keepAlive function in index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const {
 const { keepAlive } = require("./modules/keepAlive");
 const app = require("express")();
 
+keepAlive(app, 3000);
 const client = new Client(clientOptions);
 client.once("ready", ready(client));
 client.on("interactionCreate", interactionCreate(client));
 client.on("guildCreate", guildCreate());
 client.on("guildDelete", guildDelete());
 client.login(process.env["token"]);
-//keepAlive(app, 3000);
