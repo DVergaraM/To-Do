3e64e05999da8f185bc014c1be19de2a844e8a55
Dan <80771578+DVergaraM@users.noreply.github.com>
2024-06-18 18:24:44 -0500

    Update methods.js

--- modules/methods.js
+++ modules/methods.js
@@ -199,15 +199,15 @@ function run(client, rl, mode) {
       } else if (command == "start") {
         started = true;
         client.start(true);
-        client.login(process.env["prodToken"]);
+        client.login(process.env["token"]);
       } else if (command === "start" && args[1] == "true" && !started) {
         started = true;
         client.start(true);
-        client.login(process.env["prodToken"]);
+        client.login(process.env["token"]);
       } else if (command === "start" && args[1] == "false" && !started) {
         started = true;
         client.start(false);
-        client.login(process.env["prodToken"]);
+        client.login(process.env["token"]);
       } else if (command === "delete" && started) {
         client.commands(false);
       } else if (command === "create" && started) {
@@ -218,7 +218,7 @@ function run(client, rl, mode) {
     });
   } else if (mode == "prod") {
     client.start(true);
-    client.login(process.env["token"]);
+    client.login(process.env["prodToken"]);
   } else {
     console.log("Invalid mode.");
   }
